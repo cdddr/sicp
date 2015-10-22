5bbd14ccc7db6d84b5627d735faf35a02bbc6e6d
Ian Thiele <icthiele@gmail.com>
2015-10-22 15:49:55 -0500

    Through Exercies 1.36
    .

--- ch1.3.scm
+++ ch1.3.scm
@@ -200,5 +200,98 @@
     (= (gcd i b) 1))
   (filtered-accumulate relatively-prime? * 1 identity 1 inc b))
 
-(define (f g)
-  (g 2))
+;; Section 1.3.3
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+	midpoint
+	(let ((test-value (f midpoint)))
+	  (cond ((positive? test-value)
+		 (search f neg-point midpoint))
+		((negative? test-value)
+		 (search f midpoint pos-point))
+		(else midpoint))))))
+
+(define (average a b)
+  (/ (+ a b) 2))
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+	(b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+	   (search f a b))
+	  ((and (negative? b-value) (positive? a-value))
+	   (search f b a))
+	  (else
+	   (error "Values are not of opposite sign" a b)))))
+
+;; racket@> (half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3))
+;; 			       1.0 2.0)
+;; 1.89306640625
+
+;;; Fixed point function
+;;;   A function where f(x) = x. Approximated by applying f(x), f(f(x)), f(f(f(x))) until the value does not change much
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+	  next
+	  (try next))))
+  (try first-guess))
+
+;; racket@> (fixed-point cos 1.0)
+;; 0.7390822985224024
+;; racket@> (fixed-point (lambda (y) (+ (sin y) (cos y)))
+;; 		      1.0)
+;; 1.2587315962971173
+
+;; This process is similar to how square roots were computed in section 1.1.7. Using a fixed point approach for square roots requires reformulating a bit. y^{2}=x -> y = x / y
+
+(define (sqrt x)			;; does not converge. just cycles back and forth.
+  (fixed-point (lambda (y) (/ x y))
+	       1.0))
+
+(define (sqrt x)			;; this does converge, due to average damping. equivalent to 1.1.7
+  (fixed-point (lambda (y) (average y (/ x y)))
+	       1.0))
+
+
+;; Exercise 1.35 - Show that the golden ratio is a fixed point of the transformation x |-> 1 + 1/x
+;; \phi^{2} = \phi + 1 , dividing by \phi => \phi = 1 + 1/\phi, thus it is a fixed poinnt of x |-> 1 + 1/x.
+(define (golden-ratio)
+  (fixed-point (lambda (x) (+ 1 (/ 1 x)))
+	       1.0))
+
+;; Exercise 1.36 - x^{x} = 1000
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (display guess)
+    (newline)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+	  next
+	  (try next))))
+  (try first-guess))
+
+;; 34 steps
+;; 2.0, 9.965784284662087, 3.004472209841214, 6.279195757507157, 3.759850702401539, 5.215843784925895, 4.182207192401397, 4.8277650983445906, 4.387593384662677, 4.671250085763899, 4.481403616895052, 4.6053657460929, 4.5230849678718865, 4.577114682047341, 4.541382480151454, 4.564903245230833, 4.549372679303342, 4.559606491913287, 4.552853875788271, 4.557305529748263, 4.554369064436181, 4.556305311532999, 4.555028263573554, 4.555870396702851, 4.555315001192079, 4.5556812635433275, 4.555439715736846, 4.555599009998291, 4.555493957531389, 4.555563237292884, 4.555517548417651, 4.555547679306398, 4.555527808516254, 4.555540912917957, 4.555532270803653
+(define (ex1-36)
+  (fixed-point (lambda (x) (/ (log 1000) (log x)))
+	       25.0))
+
+;; 9 steps
+;; 2.0, 5.9828921423310435, 4.922168721308343, 4.628224318195455, 4.568346513136242, 4.5577305909237005, 4.555909809045131, 4.555599411610624, 4.5555465521473675, 4.555537551999825
+(define (ex1-36-damping)
+  (fixed-point (lambda (x) (average x (/ (log 1000) (log x))))
+	       25.0))
+
+;; Exercise 1.37 - k-term finite continued fraction
